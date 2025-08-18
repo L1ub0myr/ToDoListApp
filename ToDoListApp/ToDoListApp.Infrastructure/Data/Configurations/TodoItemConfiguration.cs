43f5fa2960792454e72189fa652984bf2dc55fd1
Liubomyr <bezrukyi.liubomyr@gmail.com>
2025-08-18 17:37:19 +0300

    Database context settings

---
+++ ToDoListApp/ToDoListApp.Infrastructure/Data/Configurations/TodoItemConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoListApp.Domain.Enums;
+using ToDoListApp.Domain.Models;
+
+namespace ToDoListApp.Infrastructure.Data.Configurations;
+
+public class TodoItemConfiguration : IEntityTypeConfiguration<TodoItem>
+{
+    public void Configure(EntityTypeBuilder<TodoItem> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+        builder.Property(x => x.Description)
+            .HasMaxLength(150);
+        builder.Property(x => x.TodoStatus)
+            .HasDefaultValue(TodoStatus.Todo);
+        builder.Property(x => x.IsDeleted)
+            .HasDefaultValue(false);
+    }
+}
