b6407731229a9b1e1857c5249cbc434645b5342e
Liubomyr <bezrukyi.liubomyr@gmail.com>
2025-08-18 18:11:27 +0300

    Add UnitOfWork

---
+++ ToDoListApp/ToDoListApp.Application/Abstractions/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using ToDoListApp.Application.Abstractions.Repositories;
+
+namespace ToDoListApp.Application.Abstractions.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    ITodoItemRepository TodoItemRepository { get; }
+    Task<int> CommitAsync();
+}
---
+++ ToDoListApp/ToDoListApp.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using ToDoListApp.Application.Abstractions.Repositories;
+using ToDoListApp.Application.Abstractions.UnitOfWork;
+using ToDoListApp.Infrastructure.Data;
+using ToDoListApp.Infrastructure.Repositories;
+
+namespace ToDoListApp.Infrastructure.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private TodoContext _context;
+    private bool _disposed = false;
+    private ITodoItemRepository _todoRepository;
+
+    public ITodoItemRepository TodoItemRepository => _todoRepository;
+
+    public UnitOfWork(TodoContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _todoRepository = new TodoItemRepository(_context);
+    }
+
+    public async Task<int> CommitAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    { 
+        if (!_disposed && disposing)
+            _context.Dispose();
+        _disposed = true;
+    }
+}
