0c27c089d3648b6fe20bf236eaf530f1d46ca6ab
Liubomyr <bezrukyi.liubomyr@gmail.com>
2025-08-22 12:24:14 +0300

    Add migrations

---
+++ ToDoListApp/ToDoListApp.Infrastructure/DbContexts/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListApp.Domain.Models;
+
+namespace ToDoListApp.Infrastructure.Data;
+
+public class TodoContext : DbContext
+{
+    DbSet<TodoItem> TodoItems { get; set; }
+
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoContext).Assembly);
+    }
+}
+ 
---
+++ ToDoListApp/ToDoListApp.Infrastructure/Migrations/20250819040905_InitialMigrate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoListApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    TodoStatus = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 0),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
